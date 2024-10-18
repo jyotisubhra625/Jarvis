c2f503cc26224a03bcea8e700188ac465340c38f
Frank <79392649+jyotisubhra625@users.noreply.github.com>
2024-10-18 17:24:04 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "stealth" : "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march"   : "https://www.youtube.com/watch?v=EDgaPJYe4Tg&pp=ygUUeW91dHViZSBtdXNpYyBtYWFyY2g%3D",
+    "can you hear the music" : "https://www.youtube.com/watch?v=vT8ir-GHP_U&t=294s&pp=ygUQb3BwZW5oZWltZXIgMSBocg%3D%3D",
+    "interstellar" : "https://www.youtube.com/watch?v=p2zMXSXhZ9M&t=175s&pp=ygUQaW50ZXJzdGVsbGFyIDFqcg%3D%3D"
+    
+}
